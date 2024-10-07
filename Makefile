ce2644c9ee92dc95d43f558500b15e4f940b821b
satwikmishra11 <150122188+satwikmishra11@users.noreply.github.com>
2024-10-07 18:59:46 +0530

    Add files via upload
    license and read me files

---
+++ Makefile
@@ -0,0 +1,120 @@
+.PHONY: default clean force_look qemu bochs debug mount_fat check_fat umount_fat
+
+default: thor.flp
+
+kernel/debug/kernel.bin: force_look
+	cd kernel; $(MAKE)
+
+init/debug/init.bin: force_look
+	cd init; $(MAKE)
+
+tlib/debug/libtlib.a: force_look
+	cd tlib; $(MAKE)
+
+bootloader/stage1.bin: force_look
+	cd bootloader; $(MAKE) stage1.bin
+
+bootloader/stage2.bin: force_look
+	cd bootloader; $(MAKE) stage2.bin
+
+programs: force_look tlib/debug/libtlib.a
+	cd programs/; ${MAKE} dist
+
+compile: bootloader/stage1.bin bootloader/stage2.bin init/debug/init.bin kernel/debug/kernel.bin programs
+
+hdd.img:
+	dd if=/dev/zero of=hdd.img bs=516096c count=1000
+	(echo n; echo p; echo 1; echo ""; echo ""; echo t; echo c; echo a; echo 1; echo w;) | sudo fdisk -u -C1000 -S63 -H16 hdd.img
+
+thor.flp: hdd.img bootloader/stage1.bin bootloader/stage2.bin init/debug/init.bin kernel/debug/kernel.bin programs
+	mkdir -p mnt/fake/
+	dd if=bootloader/stage1.bin of=hdd.img conv=notrunc
+	dd if=bootloader/stage2.bin of=hdd.img seek=1 conv=notrunc
+	loopdev="`/sbin/losetup -f`" && \
+	sudo /sbin/losetup -o1048576 "$$loopdev" hdd.img && \
+	sudo mkdosfs -v -F32 "$$loopdev" && \
+	sudo /bin/mount -t vfat "$$loopdev" mnt/fake/ && \
+	sudo mkdir mnt/fake/bin/ && \
+	sudo mkdir mnt/fake/sys/ && \
+	sudo mkdir mnt/fake/dev/ && \
+	sudo mkdir mnt/fake/proc/ && \
+	sudo /bin/cp init/debug/init.bin mnt/fake/ && \
+	sudo /bin/cp kernel/debug/kernel.bin mnt/fake/ && \
+	sudo /bin/cp programs/dist/* mnt/fake/bin/ && \
+	sleep 0.1 && \
+	sudo /bin/umount mnt/fake/ && \
+	sudo /sbin/losetup -d "$$loopdev"
+
+qemu: default
+	touch virtual.log
+	sudo qemu-system-x86_64 -enable-kvm -cpu host -serial file:virtual.log -netdev tap,helper=/usr/libexec/qemu-bridge-helper,id=thor_net0 -device rtl8139,netdev=thor_net0,id=thor_nic0 -vga std -hda hdd.img &
+	echo "Reading kernel log (Ctrl+C for exit)"
+	tail -f virtual.log
+	kill %1
+
+qemu_run: default
+	touch virtual.log
+	sudo run-qemu -enable-kvm -cpu host -serial file:virtual.log -vga std -hda hdd.img &
+	echo "Reading kernel log (Ctrl+C for exit)"
+	tail -f virtual.log
+	kill %1
+
+qemu_user: default
+	touch virtual.log
+	sudo qemu-system-x86_64 -enable-kvm -cpu host -serial file:virtual.log -net user,vlan=0 -net nic,model=rtl8139,vlan=0,macaddr=52:54:00:12:34:56 -net socket,listen=localhost:1234 -net dump,vlan=0,file=thor.pcap -vga std -hda hdd.img &
+	echo "Reading kernel log (Ctrl+C for exit)"
+	tail -f virtual.log
+	kill %1
+
+qemu_user_slave: default
+	touch slave.log
+	sudo qemu-system-x86_64 -enable-kvm -cpu host -serial file:slave.log -net nic,model=rtl8139,vlan=0,macaddr=52:54:00:12:34:57 -net socket,connect=localhost:1234,vlan=0 -vga std -hda hdd.img &
+	echo "Reading kernel log (Ctrl+C for exit)"
+	tail -f slave.log
+	kill %1
+
+bochs: default
+	echo "c" > commands
+	bochs -qf tools/bochsrc.txt -rc commands
+	rm commands
+
+bochs_simple: default
+	bochs -qf tools/bochsrc.txt
+
+mount_fat:
+	sudo /sbin/losetup -o1048576 /dev/loop0 hdd.img
+	sudo /bin/mount -t vfat /dev/loop0 mnt/fake/
+
+check_fat:
+	sudo /sbin/losetup -o1048576 /dev/loop0 hdd.img
+	sudo /bin/mount -t vfat /dev/loop0 mnt/fake/
+	sudo fsck.fat -v -n /dev/loop0 || true
+	sudo /bin/umount mnt/fake/
+	sudo /sbin/losetup -d /dev/loop0
+
+umount_fat:
+	sudo /bin/umount mnt/fake/
+	sudo /sbin/losetup -d /dev/loop0
+
+umount_loop:
+	sudo /sbin/losetup -d /dev/loop0
+
+debug: default
+	echo "c" > commands
+	bochs -qf tools/debug_bochsrc.txt -rc commands
+	rm commands
+
+gdb: default
+	bochs -qf tools/gdb_bochsrc.txt
+
+force_look:
+	true
+
+clean:
+	cd bootloader; $(MAKE) clean
+	cd init; $(MAKE) clean
+	cd kernel; $(MAKE) clean
+	cd programs/; $(MAKE) clean
+	cd tlib/; $(MAKE) clean
+	rm -f *.bin
+	rm -f *.flp
---
+++ cpp.mk
@@ -0,0 +1,150 @@
+CC=x86_64-elf-gcc
+CXX=x86_64-elf-g++
+AS=x86_64-elf-as
+OC=x86_64-elf-objcopy
+AR=x86_64-elf-ar
+
+WARNING_FLAGS=-Wall -Wextra -pedantic -Wold-style-cast
+COMMON_C_FLAGS=-masm=intel -I../../tstl/include/ -I../printf/include/ -I../tstl/include/ -I../tlib/include/ -Iinclude/ -nostdlib -g -Os -fno-stack-protector -fno-exceptions -funsigned-char -ffreestanding -fomit-frame-pointer -mno-red-zone -mno-3dnow -mno-mmx -fno-asynchronous-unwind-tables
+
+# Include ACPICA
+COMMON_C_FLAGS += -isystem acpica/source/include
+
+# Add more flags for C++
+COMMON_CPP_FLAGS=$(COMMON_C_FLAGS) -std=c++11 -fno-rtti
+
+DISABLE_SSE_FLAGS=-mno-sse -mno-sse2 -mno-sse3 -mno-sse4 -mno-sse4.1 -mno-sse4.2
+ENABLE_SSE_FLAGS=-msse -msse2 -msse3 -msse4 -msse4.1 -msse4.2
+
+ENABLE_AVX_FLAGS=-mavx -mavx2
+DISABLE_AVX_FLAGS=-mno-avx -mno-avx2
+
+CPP_FLAGS_LOW=-march=i386 -m32 -fno-strict-aliasing -fno-pic -fno-toplevel-reorder $(DISABLE_SSE_FLAGS) $(DISABLE_AVX_FLAGS)
+
+FLAGS_16=$(CPP_FLAGS_LOW) -mregparm=3 -mpreferred-stack-boundary=2
+FLAGS_32=$(CPP_FLAGS_LOW) -mpreferred-stack-boundary=4
+FLAGS_64=-mpreferred-stack-boundary=4 $(ENABLE_SSE_FLAGS) $(DISABLE_AVX_FLAGS)
+
+# Activate Stack Smashing Protection
+FLAGS_64 += -fstack-protector
+
+KERNEL_CPP_FLAGS_64=$(COMMON_CPP_FLAGS) $(FLAGS_64)
+
+ACPICA_C_FLAGS= $(COMMON_C_FLAGS) $(FLAGS_64) -include include/thor_acenv.hpp -include include/thor_acenvex.hpp
+
+COMMON_LINK_FLAGS=-lgcc
+
+KERNEL_LINK_FLAGS=$(COMMON_LINK_FLAGS) -T linker.ld
+
+TLIB_FLAGS=$(COMMON_CPP_FLAGS) $(FLAGS_64) $(WARNING_FLAGS) -mcmodel=small -fPIC -ffunction-sections -fdata-sections -DTHOR_TLIB=yes
+TLIB_LINK_FLAGS=$(COMMON_CPP_FLAGS) $(FLAGS_64) $(WARNING_FLAGS) -mcmodel=small -fPIC -Wl,-gc-sections
+
+PROGRAM_FLAGS=$(COMMON_CPP_FLAGS) $(FLAGS_64) $(WARNING_FLAGS) -I../../tlib/include/ -I../../printf/include/  -static -L../../tlib/debug/ -ltlib -mcmodel=small -fPIC -DTHOR_PROGRAM=yes
+PROGRAM_LINK_FLAGS=$(COMMON_CPP_FLAGS) $(FLAGS_64) $(WARNING_FLAGS) $(COMMON_LINK_FLAGS) -static -L../../tlib/debug/ -mcmodel=small -fPIC -z max-page-size=0x1000 -T ../linker.ld -Xlinker "--no-relax"
+
+NO_COLOR=\x1b[0m
+MODE_COLOR=\x1b[31;01m
+FILE_COLOR=\x1b[35;01m
+
+# Generate the rules for the assembly files of a directory
+define compile_assembly_folder
+
+debug/$(1)/%.s.o: $(1)/%.s
+	@ mkdir -p debug/$(1)/
+	@ /bin/echo -e "$(MODE_COLOR)[debug]$(NO_COLOR) Compile (assembly) $(FILE_COLOR)$(1)/$$*.s$(NO_COLOR)"
+	@ $(AS) -g -c $$< -o $$@
+
+folder_s_files := $(wildcard $(1)/*.s)
+folder_o_files := $$(folder_s_files:%.s=debug/%.s.o)
+
+O_FILES := $(O_FILES) $$(folder_o_files)
+
+endef
+
+# Generate the rules for the CPP files of a directory
+define compile_cpp_folder
+
+debug/$(1)/%.cpp.d: $(1)/%.cpp
+	@ mkdir -p debug/$(1)/
+	@ $(CXX) $(KERNEL_CPP_FLAGS_64) $(THOR_FLAGS) $(WARNING_FLAGS) -MM -MT debug/$(1)/$$*.cpp.o $$< | sed -e 's@^\(.*\)\.o:@\1.d \1.o:@' > $$@
+
+debug/$(1)/%.cpp.o: $(1)/%.cpp
+	@ mkdir -p debug/$(1)/
+	@ /bin/echo -e "$(MODE_COLOR)[debug]$(NO_COLOR) Compile $(FILE_COLOR)$(1)/$$*.cpp$(NO_COLOR)"
+	@ $(CXX) $(KERNEL_CPP_FLAGS_64) $(THOR_FLAGS) $(WARNING_FLAGS) -c $$< -o $$@
+
+folder_cpp_files := $(wildcard $(1)/*.cpp)
+folder_d_files   := $$(folder_cpp_files:%.cpp=debug/%.cpp.d)
+folder_o_files   := $$(folder_cpp_files:%.cpp=debug/%.cpp.o)
+
+D_FILES := $(D_FILES) $$(folder_d_files)
+O_FILES := $(O_FILES) $$(folder_o_files)
+
+endef
+
+# Generate the rules for the APCICA C files of a components subdirectory
+define acpica_folder_compile
+
+debug/acpica/source/components/$(1)/%.c.d: acpica/source/components/$(1)/%.c
+	@ mkdir -p debug/acpica/source/components/$(1)/
+	@ $(CXX) $(ACPICA_C_FLAGS) $(THOR_FLAGS) -MM -MT acpica/source/components/$(1)/$$*.c.o $$< | sed -e 's@^\(.*\)\.o:@\1.d \1.o:@' > $$@
+
+debug/acpica/source/components/$(1)/%.c.o: acpica/source/components/$(1)/%.c
+	@ mkdir -p debug/acpica/source/components/$(1)/
+	@ /bin/echo -e "$(MODE_COLOR)[debug]$(NO_COLOR) Compile (ACPICA) $(FILE_COLOR)$(1)/$$*.cpp$(NO_COLOR)"
+	@ $(CC) $(ACPICA_C_FLAGS) $(THOR_FLAGS) -c $$< -o $$@
+
+acpica_folder_c_files := $(wildcard acpica/source/components/$(1)/*.c)
+acpica_folder_d_files := $$(acpica_folder_c_files:%.c=debug/%.c.d)
+acpica_folder_o_files := $$(acpica_folder_c_files:%.c=debug/%.c.o)
+
+D_FILES := $(D_FILES) $$(acpica_folder_d_files)
+O_FILES := $(O_FILES) $$(acpica_folder_o_files)
+
+endef
+
+# Generate the rules for the CPP files of a directory
+define program_compile_cpp_folder
+
+debug/$(1)/%.cpp.o: $(1)/%.cpp
+	@ mkdir -p debug/$(1)/
+	@ /bin/echo -e "$(MODE_COLOR)[debug]$(NO_COLOR) Compile (program) $(FILE_COLOR)$(1)/$$*.cpp$(NO_COLOR)"
+	@ $(CXX) -c $$< -o $$@ $(PROGRAM_FLAGS)
+
+folder_cpp_files := $(wildcard $(1)/*.cpp)
+folder_o_files   := $$(folder_cpp_files:%.cpp=debug/%.cpp.o)
+
+O_FILES := $(O_FILES) $$(folder_o_files)
+
+endef
+
+define program_link_executable
+
+debug/$(1): $(O_FILES)
+	@ mkdir -p debug/
+	@ /bin/echo -e "$(MODE_COLOR)[debug]$(NO_COLOR) Link (program) $(FILE_COLOR)$$@$(NO_COLOR)"
+	@ $(CXX) -o debug/$(1) $(PROGRAM_LINK_FLAGS) ../../tlib/debug/src/crti.s.o $$(shell $(CXX) -print-file-name=crtbegin.o) $(O_FILES) -ltlib $$(shell $(CXX) -print-file-name=crtend.o) ../../tlib/debug/src/crtn.s.o
+link: debug/$(1)
+
+endef
+
+# Generate the rules for the CPP files of a directory
+define tlib_compile_cpp_folder
+
+debug/$(1)/%.cpp.d: $(1)/%.cpp
+	@ mkdir -p debug/$(1)/
+	@ $(CXX) $(TLIB_FLAGS) -MM -MT debug/$(1)/$$*.cpp.o $(1)/$$*.cpp | sed -e 's@^\(.*\)\.o:@\1.d \1.o:@' > $$@
+
+debug/$(1)/%.cpp.o: $(1)/%.cpp
+	@ mkdir -p debug/$(1)/
+	@ /bin/echo -e "$(MODE_COLOR)[debug]$(NO_COLOR) Compile (tlib) $(FILE_COLOR)$(1)/$$*.cpp$(NO_COLOR)"
+	@ $(CXX) $(TLIB_FLAGS) -c $$< -o $$@
+
+folder_cpp_files := $(wildcard $(1)/*.cpp)
+folder_d_files   := $$(folder_cpp_files:%.cpp=debug/%.cpp.d)
+folder_o_files   := $$(folder_cpp_files:%.cpp=debug/%.cpp.o)
+
+D_FILES := $(D_FILES) $$(folder_d_files)
+O_FILES := $(O_FILES) $$(folder_o_files)
+
+endef
